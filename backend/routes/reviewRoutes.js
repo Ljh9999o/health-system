b65b376222b07a062b5d97088a13d44fbda32028
Ljh <1363091898@qq.com>
2025-03-18 20:28:58 +0800

    创建评价路由

---
+++ backend/routes/reviewRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const reviewController = require('../controllers/reviewController');
+
+const router = express.Router();
+
+// 创建评价
+router.post('/', reviewController.createReview);
+
+// 查询医生的评价
+router.get('/:doctor_id', reviewController.getReviewsByDoctorId);
+
+// 删除评价
+router.delete('/:id', reviewController.deleteReview);
+
+module.exports = router;

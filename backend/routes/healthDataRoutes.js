3ac55aa1df345d4faedeab9fcd2a658afc62b3fa
Ljh <1363091898@qq.com>
2025-03-19 15:32:47 +0800

    创建健康数据路由

---
+++ backend/routes/healthDataRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const healthDataController = require('../controllers/healthDataController');
+
+const router = express.Router();
+
+// 记录健康数据
+router.post('/', healthDataController.recordData);
+
+// 查询用户的健康数据
+router.get('/:user_id', healthDataController.getDataByUserId);
+
+// 查询用户的健康数据统计
+router.get('/:user_id/statistics', healthDataController.getStatisticsByUserId);
+
+module.exports = router;
